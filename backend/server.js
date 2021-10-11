8d3f0cd169df7ff9a0f2fd0a92148304a3554d13
sidharth1211 <communicate.sidharth@gmail.com>
2021-10-11 12:25:30 +0530

    backend api for product and product id

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 const express = require("express")
 const app = express();
+const products = require('./data/products')
 
-app.get('/')
 
+
+app.get('/',(req,res)=>{
+    res.send("API is running ")
+})
+
+
+app.get('/api/product',(req,res)=>{
+    res.json(products);
+})
+
+app.get('/api/product/:id',(req,res)=>{
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product);
+})
 app.listen(5000,console.log("app running on port 5000"))
